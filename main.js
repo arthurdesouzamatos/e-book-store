7e23a7abef9375c7ec02a6c5f4bf23606625cefe
Arthur de Souza Matos <103154624+arthurdesouzamatos@users.noreply.github.com>
2023-11-14 17:40:03 -0300

    v2.0

---
+++ main.js
@@ -0,0 +1,10 @@
+function like() {
+    let like = document.getElementById("teste");
+
+if(like.className == 'bi bi-heart') {
+    like.className = 'bi bi-heart-fill';
+} else {
+    like.className = 'bi bi-heart';
+}
+
+}
